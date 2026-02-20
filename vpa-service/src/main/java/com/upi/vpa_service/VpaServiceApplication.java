9b5dfff09925078262ef854c6d1e2a654b2171cf
adhishpawar <adhishpawar@gmail.com>
2026-02-21 01:51:18 +0530

    service and DB conn init

---
+++ vpa-service/src/main/java/com/upi/vpa_service/VpaServiceApplication.java
@@ -0,0 +1,12 @@
+package com.upi.vpa_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpaServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(VpaServiceApplication.class, args);
+	}
+
+}
